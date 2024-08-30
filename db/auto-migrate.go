0817525c26d21271594ca2d1592f13916ca258ab
Vector-ops <sumith2827@gmail.com>
2024-08-30 19:18:21 +0530

    feat: add auto migrate and init db

---
+++ db/auto-migrate.go
@@ -0,0 +1,10 @@
+package db
+
+import (
+	"github.com/vector-ops/ticketeer/models"
+	"gorm.io/gorm"
+)
+
+func DBMigrator(db *gorm.DB) error {
+	return db.AutoMigrate(&models.Event{}, &models.Ticket{}, &models.User{})
+}
