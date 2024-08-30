5a496fab4e0d37472d51a8faf39ddc0a1352b748
Vector-ops <sumith2827@gmail.com>
2024-08-30 19:18:38 +0530

    feat: add event handler

---
+++ handlers/event.go
@@ -0,0 +1,141 @@
+package handlers
+
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/vector-ops/ticketeer/models"
+)
+
+type EventHandler struct {
+	repository models.EventRepository
+}
+
+func NewEventHandler(router fiber.Router, repository models.EventRepository) {
+	handler := &EventHandler{
+		repository: repository,
+	}
+
+	router.Get("/", handler.GetMany)
+	router.Get("/:eventId", handler.GetOne)
+	router.Post("/", handler.CreateOne)
+	router.Put("/:eventId", handler.UpdateOne)
+	router.Delete("/:eventId", handler.DeleteOne)
+}
+
+func (h *EventHandler) GetMany(ctx *fiber.Ctx) error {
+	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	defer cancel()
+
+	events, err := h.repository.GetMany(context)
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+			"status":  "fail",
+			"message": err.Error(),
+			"data":    nil,
+		})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(&fiber.Map{
+		"status":  "success",
+		"message": "",
+		"data":    events,
+	})
+}
+func (h *EventHandler) GetOne(ctx *fiber.Ctx) error {
+	eventId, _ := strconv.Atoi(ctx.Params("eventId"))
+
+	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	defer cancel()
+
+	event, err := h.repository.GetOne(context, uint(eventId))
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+			"status":  "fail",
+			"message": err.Error(),
+			"data":    nil,
+		})
+	}
+	return ctx.Status(fiber.StatusOK).JSON(&fiber.Map{
+		"status":  "success",
+		"message": "",
+		"data":    event,
+	})
+
+}
+func (h *EventHandler) CreateOne(ctx *fiber.Ctx) error {
+	event := &models.Event{}
+	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	defer cancel()
+
+	if err := ctx.BodyParser(event); err != nil {
+		return ctx.Status(fiber.StatusUnprocessableEntity).JSON(&fiber.Map{
+			"status":  "fail",
+			"message": err.Error(),
+			"data":    nil,
+		})
+	}
+
+	event, err := h.repository.CreateOne(context, event)
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+			"status":  "fail",
+			"message": err.Error(),
+			"data":    nil,
+		})
+	}
+
+	return ctx.Status(fiber.StatusCreated).JSON(&fiber.Map{
+		"status":  "success",
+		"message": "Event created",
+		"data":    event,
+	})
+}
+
+func (h *EventHandler) UpdateOne(ctx *fiber.Ctx) error {
+	eventId, _ := strconv.Atoi(ctx.Params("eventid"))
+	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	defer cancel()
+
+	updateEvent := make(map[string]interface{})
+	if err := ctx.BodyParser(&updateEvent); err != nil {
+		return ctx.Status(fiber.StatusUnprocessableEntity).JSON(&fiber.Map{
+			"status":  "fail",
+			"message": err.Error(),
+			"data":    nil,
+		})
+	}
+
+	event, err := h.repository.UpdateOne(context, uint(eventId), updateEvent)
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+			"status":  "fail",
+			"message": err.Error(),
+			"data":    nil,
+		})
+	}
+	return ctx.Status(fiber.StatusOK).JSON(&fiber.Map{
+		"status":  "success",
+		"message": "Updated event",
+		"data":    event,
+	})
+}
+
+func (h *EventHandler) DeleteOne(ctx *fiber.Ctx) error {
+	eventId, _ := strconv.Atoi(ctx.Params("eventId"))
+
+	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	defer cancel()
+
+	err := h.repository.DeleteOne(context, uint(eventId))
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+			"status":  "fail",
+			"message": err.Error(),
+			"data":    nil,
+		})
+	}
+	return ctx.SendStatus(fiber.StatusNoContent)
+}
